1c62cc6842896ab2e28eb044aabe2ac25fa9088e
Diogo Dos Santos Lessa <diogodslessa@gmail.com>
2025-03-04 16:59:13 -0300

    corrigido ingestao raw

--- Airflow/dags/ingestion_raw.py
+++ Airflow/dags/ingestion_raw.py
@@ -12,7 +12,8 @@ default_args = {
 dag = DAG(
     dag_id='Ingestion_raw',
     default_args=default_args,
-    schedule_interval='*/10 * * * *',
+    schedule_interval=None,
+    # schedule_interval='*/10 * * * *',
     catchup=False,
     tags=['airflow','simulation','ingestion']
 ) 
@@ -29,15 +30,15 @@ def parse_cdc_event(data):
     valores_extraidos = re.findall(r":(?:'([^']*)'|(\d+(?:\.\d+)?))", colunas_raw)
     valores_final = [v[0] if v[0] else v[1] for v in valores_extraidos]
     valores = dict(zip(colunas, valores_final))
-    valores["tabela"] = tabela
     valores["operacao"] = operacao
 
-    return valores
+    return tabela, valores
 
 def pull_to_MinIO(
         schema,
         tabela,
-        files_source_table
+        files_source_table,
+        file_format
     ):
     """
     pull all data to raw in MinIO
@@ -48,45 +49,58 @@ def pull_to_MinIO(
     
     for file in files_source_table:
         path = f'/{schema}/{tabela}'
-        minio.upload_file('raw',path,'parquet',file)
+        minio.upload_file('raw',path,file_format,file)
         os.remove(file)
 
 
 def ingest_from_postgres():
     from include.postgres_manager import Postgres_Connector
-    import pandas as pd, os, glob, re
-
+    import json, os, glob
+    root = f'{Variable.get("root_data")}/postgres_ingestion/cdc/'
     postgres = Postgres_Connector()
     postgres.open_connection()
     cursor = postgres.get_cursor()
 
     cursor.execute("SELECT * FROM pg_logical_slot_get_changes('python_slot', NULL, NULL);")
     changes = cursor.fetchall()
-    eventos = []
+
     if changes:
-        
         for change in changes:
             lsn, xid, data = change
-            dados = parse_cdc_event(data)
-            if dados is not None:
-                eventos.append(dados)
+            resultado = parse_cdc_event(data)
+            if resultado is not None:
+                tabela, dados = resultado
+                caminho = f'{root}{tabela}'
+                os.makedirs(caminho,exist_ok=True)
+                with open(f'{caminho}/{datetime.now().strftime('%y-%m-%d-%H-%M-%S-%f')}.json', "w", encoding="utf-8") as f:
+                    json.dump(dados, f, indent=4, ensure_ascii=False)
+
     postgres.close_cursor(cursor)
     postgres.close_connection()
 
-    root = f'{Variable.get("root_data")}/postgres_ingestion/cdc/'
-    if len(eventos) > 0:
-        df = pd.DataFrame(eventos)
-        os.makedirs(root,exist_ok=True)
-        df.to_parquet(root,partition_cols=['tabela'],index=False)
-
     tabelas = glob.glob(f'{root}*')
     for tabela_path in tabelas:
-        files = glob.glob(f'{tabela_path}/*.parquet')
-        match = re.search(r'tabela=([^/]+)', tabela_path)
-        tabela = ''
-        if match:
-            tabela = match.group(1)
-            pull_to_MinIO('postgres/cdc/',tabela,files)
+        files = glob.glob(f'{tabela_path}/*.json')
+        tabela = tabela_path.split(root)[1]
+
+        dados_combinados = []
+        for file in files:
+            with open(file, 'r', encoding='utf-8') as f:
+                try:
+                    dados = json.load(f)
+                    dados_combinados.append(dados)
+                except json.JSONDecodeError as e:
+                    print(f"Erro ao ler {file}: {e}")
+
+
+        output_file = os.path.join(tabela_path, "merged.json")
+        with open(output_file, 'w', encoding='utf-8') as f:
+            json.dump(dados_combinados, f, ensure_ascii=False, indent=4)
+
+        pull_to_MinIO('postgres/cdc/', tabela, [output_file], 'json')
+
+        for file in files:
+            os.remove(file)
 
 def full_load_postgres():
     from include.postgres_manager import Postgres_Connector
@@ -108,7 +122,7 @@ def full_load_postgres():
         os.makedirs(path,exist_ok=True)
         df.to_parquet(f'{path}{date}.parquet',index=False)
 
-        pull_to_MinIO('/postgres/full_load',tabela,glob.glob(f'{path}*.parquet'))
+        pull_to_MinIO('/postgres/full_load',tabela,glob.glob(f'{path}*.parquet'),'parquet')
 
 
     postgres.close_cursor(cursor)
@@ -134,8 +148,6 @@ ingest_from_postgres
 
 
 # full_load_postgres >> ingest_from_postgres
-
-ingest_from_postgres
 
 
 
